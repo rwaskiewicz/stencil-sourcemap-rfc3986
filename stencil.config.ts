a7bd061da62f219b959f79023f7048aa01877767
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-10-11 08:41:34 -0400

    initial output from `npm init stencil`

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'rfc-3986-example',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
